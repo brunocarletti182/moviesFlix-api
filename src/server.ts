7e6c2a0787ba3c32e624c2ef9d50317746c4785c
Bruno Carletti <brunocarletti182@gmail.com>
2024-03-03 19:32:27 -0300

    updating movie

--- src/server.ts
+++ src/server.ts
@@ -38,6 +38,35 @@ app.post("/movies", async (req, res) => {
     }
     res.status(201).send();
 });
+
+app.put("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try{
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!movie) {
+            return res.status(404).send({message: "Movie not found"});
+        }
+
+        const data = {...req.body};
+        data.release_date = data.release_date ? new Date(data.release_date): undefined;
+
+        await prisma.movie.update ({
+            where: {
+                id
+            }, 
+            data: data
+        });
+    } catch (error) {
+        return res.status(500).send({message: "error when updating movie"});
+    }
+    res.status(200).send();
+});
     
 
 app.listen(port, () => {
